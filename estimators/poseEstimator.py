d7e30b8136027766e9c7ed7368da01483559c0d6
Jan Sieber <uni@email-master.de>
2019-07-08 18:51:58 +0200

    added face estimator

--- estimators/poseEstimator.py
+++ estimators/poseEstimator.py
@@ -1,9 +1,33 @@
 import cv2
+import dlib
+import imutils
 
 
+class FacePoseEstimator:
+    def __init__(self):
+        # initialize dlib's face detector (HOG-based) and then create
+        # the facial landmark predictor
+        self.detector = dlib.get_frontal_face_detector()
+        self.predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
+
+    def predict(self, imPath):
+        faces = []
+        image = cv2.imread(imPath)
+        # reshape?
+        height, width = image.shape[0], image.shape[1]
+        image = imutils.resize(image, width=500)
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        rects = self.detector(gray, 1)
+        for (i, rect) in enumerate(rects):
+            faces.append([])
+            shape = self.predictor(gray, rect)
+            for j in range(0, 68):
+                x, y = shape.part(j).x*width/image.shape[1], shape.part(j).y*height/image.shape[0]
+                faces[-1].append((int(x), int(y)))
+        return faces
 
 
-class PoseEstimator:
+class BodyPoseEstimator:
     def __init__(self):
         # Specify the paths for the 2 files
         protoFile = "pose/mpi/pose_deploy_linevec_faster_4_stages.prototxt"
@@ -13,9 +37,8 @@ class PoseEstimator:
         self.net = cv2.dnn.readNetFromCaffe(protoFile, weightsFile)
 
     def predict(self, imPath):
-
         # Read image
-        frame = cv2.imread("single.jpg")
+        frame = cv2.imread(imPath)
         frameWidth = frame.shape[1]
         frameHeight = frame.shape[0]
         # Specify the input image dimensions
@@ -44,3 +67,8 @@ class PoseEstimator:
 
             points.append((x, y))
         return points
+
+
+if __name__ == "__main__":
+    face = FacePoseEstimator()
+    marks = face.predict("testFace.jpg")
