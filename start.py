27e28a198251ed7574e16637695da1727975b9c1
Berndinio <uni@email-master.de>
2019-07-12 19:28:30 +0200

    !!!!!!!!!!!!!!!!!FINAL VERSION 1.0
    Former-commit-id: ba51433dd808f24b160c8fbed57030bdb21d03f6

--- start.py
+++ start.py
@@ -18,11 +18,15 @@ import matplotlib.pyplot as plt
 
 
 class Trainer:
-    def __init__(self, sampleImage, numBlocks, upsampleType=1):
+    def __init__(self, sampleImage, numBlocks, upsampleType=1, maskType=1):
         self.ceLoss = torch.nn.BCELoss()
         self.sampleImage = sampleImage
         s = sampleImage.shape
         self.upsampleType = upsampleType
+        self.maskType = maskType
+        # maskType = 0 or 1
+        # 0 = keypoints
+        # 1 = BBox
         # flag = G1 or G2
         # G1 = With FC in mid
         # G2 = With Conv in mid
@@ -59,7 +63,7 @@ class Trainer:
         loss += self.ceLoss(outD.float().to(Utils.g_device), torch.ones(outD.shape).float().to(Utils.g_device))
         return loss
 
-    def startTraining(self, numEpochs=10, maskType=0):
+    def startTraining(self, numEpochs=10, trainingStage=0, loaderPrefix="factor4_upsampling_bbox"):
         """
         :param maskType:    0=Landmark, 1=BBox
         """
@@ -69,123 +73,176 @@ class Trainer:
         ])
 
         dataset = FEIPostDataset(root="dataset/FEI/", sampleShape=self.sampleImage.shape,
-                                 maskType=maskType, transform=data_transform)
+                                 maskType=self.maskType, transform=data_transform)
         dataLoader = torch.utils.data.DataLoader(dataset,
                                                  batch_size=4, shuffle=False,
                                                  num_workers=4)
         print("Loaded Data")
-        # stage 1
-        optimizer = torch.optim.Adam(self.G1.parameters(), lr=0.00001, betas=(0.5, 0.999))
-        epochLosses = []
-        for epoch in range(numEpochs):
-            print("Running stage 1 training epoch " + str(epoch)+"/"+str(numEpochs))
-            allLoss = 0
-            #path of plots
-            mType = "BBox"
-            if maskType == 0:
-                mType = "keypoints"
-            up = "deconv"
-            if self.upsampleType == 1:
-                up = "upsample"
-            folderPath = "results/"+mType+"_"+up+"/"
-            print(folderPath)
-            import os
-            if not os.path.exists(folderPath):
-                os.makedirs(folderPath)
-            #begin training
-            running_loss = 0
-            for i, (conditionImages, targetImages, masks, embeddings) in enumerate(dataLoader, 0):
-                conditionImages, targetImages, masks, embeddings = conditionImages.to(Utils.g_device), targetImages.to(Utils.g_device), masks.to(Utils.g_device), embeddings.to(Utils.g_device)
-                inputs = torch.cat((conditionImages, embeddings), dim=1)
-                # zero the parameter gradients
-                optimizer.zero_grad()
-                # forward + backward + optimize
-                outputs = self.G1(inputs)
 
-                outputs = outputs[:, :3]
-                loss = self.lossG1(outputs, targetImages, masks)
-                loss.backward()
-                optimizer.step()
+        #path of plots
+        mType = "BBox"
+        if self.maskType == 0:
+            mType = "keypoints"
+        up = "deconv"
+        if self.upsampleType == 1:
+            up = "upsample"
+        folderPath = "results/"+mType+"_"+up+"/"
+        print(folderPath)
 
-                # print statistics
-                running_loss += loss.item()
-                allLoss += loss.item()
-
-                if i==0:
-                    running_loss = 0
-                if i % 20 == 0 and not i == 0:  # print every 2000 mini-batches
-                    print('[%d, %5d] loss: %.3f' %
-                          (epoch + 1, i + 1, running_loss / 2000))
-                    running_loss = 0.0
-                    # save images
-                    for x, img in enumerate(outputs):
-                        toSave = transforms.ToPILImage(mode="RGB")(img.cpu())
-                        toSave.save(folderPath+"1G-generated"+str(x)+".png")
-                        toSave = transforms.ToPILImage(mode="RGB")(conditionImages[x].cpu())
-                        toSave.save(folderPath+"1G-conditionImages"+str(x)+".png")
-                        toSave = transforms.ToPILImage(mode="RGB")(targetImages[x].cpu())
-                        toSave.save(folderPath+"1G-target"+str(x)+".png")
-            epochLosses.append(allLoss/(len(dataLoader) * self.sampleImage.shape[0] * self.sampleImage.shape[1] * self.sampleImage.shape[2]))
-            plt.plot(epochLosses)
-            plt.title(mType+" "+up)
-            plt.savefig(folderPath+"epochLoss.png")
-            plt.clf()
-            if epoch%(numEpochs/10)==0 and not epoch==0:
-                torch.save(self.G1, folderPath+"G1-"+("%.3f"%epochLosses[-1])+"-"+str(epoch)+".pt")
-        torch.save(self.G1, folderPath+"G1-"+("%.3f"%epochLosses[-1])+"-"+str(numEpochs)+".pt")
-
-        # stage 2
-        optimizer = torch.optim.Adam(list(self.G2.parameters()) + list(self.D.parameters()), lr=0.00001,
-                                     betas=(0.5, 0.999))
-        for epoch in range(0):
-            print("Running stage 2 training epoch " + str(epoch)+"/"+str(numEpochs))
-            running_loss = 0
-            for i, (conditionImages, targetImages, masks, embeddings) in enumerate(dataLoader, 0):
-                conditionImages, targetImages, masks, embeddings = conditionImages.to(Utils.g_device), targetImages.to(Utils.g_device), masks.to(Utils.g_device), embeddings.to(Utils.g_device)
-                ran = random.random()
-                if ran >= 0.5:
+        # stage 0
+        if(trainingStage <= 0):
+            print("Training G1")
+            optimizer = torch.optim.Adam(self.G1.parameters(), lr=0.00001, betas=(0.5, 0.999))
+            epochLosses = []
+            for epoch in range(numEpochs):
+                print("Running stage 1 training epoch " + str(epoch)+"/"+str(numEpochs))
+                allLossInEpoch = 0
+                import os
+                if not os.path.exists(folderPath):
+                    os.makedirs(folderPath)
+                #begin training
+                running_loss = 0
+                for i, (conditionImages, targetImages, masks, embeddings) in enumerate(dataLoader, 0):
+                    conditionImages, targetImages, masks, embeddings = conditionImages.to(Utils.g_device), targetImages.to(Utils.g_device), masks.to(Utils.g_device), embeddings.to(Utils.g_device)
                     inputs = torch.cat((conditionImages, embeddings), dim=1)
                     # zero the parameter gradients
                     optimizer.zero_grad()
-
-                    # forward things
+                    # forward + backward + optimize
                     outputs = self.G1(inputs)
+
                     outputs = outputs[:, :3]
-                    inputs2 = torch.cat((conditionImages, outputs), dim=1)
-                    outputs2 = self.G2(inputs2)
-                    outputs2 = outputs2[:, 1:4]
-                    outputs3 = outputs2 + outputs
-                    # generate fake Pair
-                    generated = outputs3
-                    condition = conditionImages
-                    targetDecision = torch.zeros((condition.shape[0], 1)).to(Utils.g_device)
-                else:
-                    # generate real pair
-                    generated = targetImages
-                    condition = conditionImages
-                    targetDecision = torch.ones((condition.shape[0], 1)).to(Utils.g_device)
+                    loss = self.lossG1(outputs, targetImages, masks)
+                    loss.backward()
+                    optimizer.step()
 
-                discriminatorPair = torch.cat((generated, condition), dim=1)
-                decision = self.D(discriminatorPair)
-                # loss of D
-                loss = self.lossD(decision, targetDecision).to(Utils.g_device)
-                # loss of G2
-                if targetDecision[0, 0] == 0:
-                    loss = loss + self.lossG2(generated, targetImages, masks, decision, lamb=2)
-                loss.backward()
-                optimizer.step()
+                    # print statistics
+                    running_loss += loss.item()
+                    allLossInEpoch += loss.item()
 
-                # print statistics
-                running_loss += loss.item()
-                if i % 20 == 0 and not i == 0:  # print every 2000 mini-batches
-                    print('[%d, %5d] loss: %.3f' %
-                          (epoch + 1, i + 1, running_loss / 2000))
-                    running_loss = 0.0
-                    # save images
-                    for x, img in enumerate(outputs):
-                        toSave = transforms.ToPILImage(mode="RGB")(img.cpu())
-                        toSave.save("results/2G-"+str(x)+".png")
+                    if i==0:
+                        running_loss = 0
+                    if i % 20 == 0 and not i == 0:  # print every 2000 mini-batches
+                        print('[%d, %5d] loss: %.3f' %
+                              (epoch + 1, i + 1, running_loss / 2000))
+                        running_loss = 0.0
+                        # save images
+                        for x, img in enumerate(outputs):
+                            toSave = transforms.ToPILImage(mode="RGB")(img.cpu())
+                            toSave.save(folderPath+"1G-generated"+str(x)+".png")
+                            toSave = transforms.ToPILImage(mode="RGB")(conditionImages[x].cpu())
+                            toSave.save(folderPath+"1G-conditionImages"+str(x)+".png")
+                            toSave = transforms.ToPILImage(mode="RGB")(targetImages[x].cpu())
+                            toSave.save(folderPath+"1G-target"+str(x)+".png")
+                epochLosses.append(allLossInEpoch/(len(dataLoader) * self.sampleImage.shape[0] * self.sampleImage.shape[1] * self.sampleImage.shape[2]))
+                plt.plot(epochLosses)
+                plt.title(mType+" "+up)
+                plt.savefig(folderPath+"epochLossG1.png")
+                plt.clf()
+                if epoch%(numEpochs/10)==0 and not epoch==0:
+                    torch.save(self.G1, folderPath+"G1-"+("%.3f"%epochLosses[-1])+"-"+str(epoch)+".pt")
+            torch.save(self.G1, folderPath+"G1-"+("%.3f"%epochLosses[-1])+"-"+str(numEpochs)+".pt")
+        else:
+            print("Loading G1")
+            self.G1 = torch.load("results/"+loaderPrefix+"/G1.pt")
+        # stage 1
+        if(trainingStage <= 1):
+            optimizer = torch.optim.Adam(list(self.G2.parameters()) + list(self.D.parameters()), lr=0.00001,
+                                         betas=(0.5, 0.999))
+            epochLosses = []
+            for epoch in range(numEpochs):
+                print("Running stage 2 training epoch " + str(epoch)+"/"+str(numEpochs))
+                running_loss = 0
+                allLossInEpoch = 0
+                import os
+                if not os.path.exists(folderPath):
+                    os.makedirs(folderPath)
+                for i, (conditionImages, targetImages, masks, embeddings) in enumerate(dataLoader, 0):
+                    conditionImages, targetImages, masks, embeddings = conditionImages.to(Utils.g_device), targetImages.to(Utils.g_device), masks.to(Utils.g_device), embeddings.to(Utils.g_device)
+                    ran = random.random()
+                    if ran >= 0.3:
+                        inputs = torch.cat((conditionImages, embeddings), dim=1)
+                        # zero the parameter gradients
+                        optimizer.zero_grad()
 
+                        # forward things
+                        outputs = self.G1(inputs)
+                        outputs = outputs[:, :3]
+                        inputs2 = torch.cat((conditionImages, outputs), dim=1)
+                        outputs2 = self.G2(inputs2)
+                        outputs2 = outputs2[:, 1:4]
+                        outputs3 = outputs2 + outputs
+                        # generate fake Pair
+                        generated = outputs3
+                        condition = conditionImages
+                        targetDecision = torch.zeros((condition.shape[0], 1)).to(Utils.g_device)
+                    else:
+                        # generate real pair
+                        generated = targetImages
+                        condition = conditionImages
+                        targetDecision = torch.ones((condition.shape[0], 1)).to(Utils.g_device)
+
+                    discriminatorPair = torch.cat((generated, condition), dim=1)
+                    decision = self.D(discriminatorPair)
+                    # loss of D
+                    loss = self.lossD(decision, targetDecision).to(Utils.g_device)
+                    # loss of G2
+                    if targetDecision[0, 0] == 0:
+                        loss = loss + self.lossG2(generated, targetImages, masks, decision, lamb=2)
+                    loss.backward()
+                    optimizer.step()
+
+                    # print statistics
+                    running_loss += loss.item()
+                    allLossInEpoch += loss.item()
+                    if i % 20 == 0 and not i == 0:  # print every 2000 mini-batches
+                        print('[%d, %5d] loss: %.3f' %
+                              (epoch + 1, i + 1, running_loss / 2000))
+                        running_loss = 0.0
+                        # save images
+                        for x, img in enumerate(generated):
+                            toSave = transforms.ToPILImage(mode="RGB")(img.cpu())
+                            toSave.save(folderPath+"2G-generated-fromG2-"+str(x)+".png")
+                            toSave = transforms.ToPILImage(mode="RGB")(outputs[x].cpu())
+                            toSave.save(folderPath+"2G-generated-fromG1-"+str(x)+".png")
+                            toSave = transforms.ToPILImage(mode="RGB")(condition[x].cpu())
+                            toSave.save(folderPath+"2G-conditionImages-"+str(x)+".png")
+                            toSave = transforms.ToPILImage(mode="RGB")(targetImages[x].cpu())
+                            toSave.save(folderPath+"2G-target-"+str(x)+".png")
+                epochLosses.append(allLossInEpoch/(len(dataLoader) * self.sampleImage.shape[0] * self.sampleImage.shape[1] * self.sampleImage.shape[2]))
+                plt.plot(epochLosses)
+                plt.title(mType+" "+up)
+                plt.savefig(folderPath+"epochLossG2.png")
+                plt.clf()
+                if epoch%(numEpochs/10)==0 and not epoch==0:
+                    torch.save(self.G2, folderPath+"G2-"+("%.3f"%epochLosses[-1])+"-"+str(epoch)+".pt")
+                    torch.save(self.D, folderPath+"D-"+("%.3f"%epochLosses[-1])+"-"+str(epoch)+".pt")
+            torch.save(self.G2, folderPath+"G2-"+("%.3f"%epochLosses[-1])+"-"+str(numEpochs)+".pt")
+            torch.save(self.D, folderPath+"D-"+("%.3f"%epochLosses[-1])+"-"+str(epoch)+".pt")
+        else:
+            print("Loading G2 and D")
+            self.G2 = torch.load("results/"+loaderPrefix+"/G2.pt")
+            self.D = torch.load("results/"+loaderPrefix+"/D.pt")
+        # stage 2 ==> just generate some
+        if(trainingStage <= 2):
+            print("Running stage 3. Generating...")
+            import os
+            if not os.path.exists(folderPath):
+                os.makedirs(folderPath)
+            for i, (conditionImages, targetImages, masks, embeddings) in enumerate(dataLoader, 0):
+                conditionImages, targetImages, masks, embeddings = conditionImages.to(Utils.g_device), targetImages.to(Utils.g_device), masks.to(Utils.g_device), embeddings.to(Utils.g_device)
+                inputs = torch.cat((conditionImages, embeddings), dim=1)
+
+                # forward things
+                outputs = self.G1(inputs)
+                outputs = outputs[:, :3]
+                inputs2 = torch.cat((conditionImages, outputs), dim=1)
+                outputs2 = self.G2(inputs2)
+                outputs2 = outputs2[:, 1:4]
+                generated = outputs2 + outputs
+
+                for x, img in enumerate(generated):
+                    toSave = transforms.ToPILImage(mode="RGB")(img.cpu())
+                    toSave.save(folderPath+"generated-"+str(i*100+x)+".png")
 
 if __name__ == "__main__":
     s = (3, 480, 640)
@@ -201,4 +258,4 @@ if __name__ == "__main__":
     #=============================>
     for up in [1]:
         for mask in [1]:
-            Trainer(sample, 6, up).startTraining(500, mask)
+            Trainer(sample, 6, up, mask).startTraining(50, 2, "factor4_upsampling_bbox")
